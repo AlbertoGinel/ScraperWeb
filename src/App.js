4080d88a34ca161d670b1017f36af8351298d5fb
Alberto Ginel <aginelsalvador@gmail.com>
2023-12-17 01:40:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ScraperComponent from "./components/ScraperComponent";
+
+function App() {
+  return (
+    <div>
+      <ScraperComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ScraperComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ScraperComponent = () => {
+  return <div>ScraperComponent</div>;
+};
+
+export default ScraperComponent;
