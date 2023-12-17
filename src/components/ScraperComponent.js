eb573963c71652d781da5a293a9db1d899a8c35b
Alberto Ginel <aginelsalvador@gmail.com>
2023-12-17 21:30:25 +0200

    Filter options

--- src/components/ScraperComponent.js
+++ src/components/ScraperComponent.js
@@ -1,18 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useWebScraper from "../hooks/useWebScraper";
 import ArticleElement from "./ArticleElement";
 
 const ScraperComponent = () => {
   const { scrapedData, isLoading, error, refetch } = useWebScraper();
 
+  const [selectedOption, setSelectedOption] = useState("option1");
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  useEffect(() => {
+    // Filter the data based on the selected option
+    if (selectedOption === "option1") {
+      setFilteredData(scrapedData); //original
+    } else if (selectedOption === "option2") {
+      setFilteredData(
+        scrapedData
+          .filter((item) => item.title.split(" ").length > 5)
+          .sort((a, b) => b.comments - a.comments)
+      );
+    } else if (selectedOption === "option3") {
+      setFilteredData(
+        scrapedData
+          .filter((item) => item.title.split(" ").length <= 5)
+          .sort((a, b) => b.points - a.points)
+      );
+    } else {
+      setFilteredData(scrapedData);
+    }
+  }, [selectedOption, scrapedData]);
+
   return (
     <div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
 
-      {scrapedData.length > 0 && (
+      <div className="flex flex-col space-y-4">
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={selectedOption === "option1"}
+            onChange={() => handleOptionChange("option1")}
+            className="mr-2"
+          />
+          No filter
+        </label>
+
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={selectedOption === "option2"}
+            onChange={() => handleOptionChange("option2")}
+            className="mr-2"
+          />
+          Entries with more than five words in the title ordered by the number
+          of comments first.
+        </label>
+
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={selectedOption === "option3"}
+            onChange={() => handleOptionChange("option3")}
+            className="mr-2"
+          />
+          Entries with less than or equal to five words in the title ordered by
+          points.
+        </label>
+
+        {false && (
+          <p className="font-bold">
+            Selected Option: {selectedOption || "None"}
+          </p>
+        )}
+      </div>
+
+      <p>List Length: {filteredData.length}</p>
+
+      {filteredData.length > 0 && (
         <ul>
-          {scrapedData.map((item, index) => (
+          {filteredData.map((item, index) => (
             <ArticleElement key={index} item={item} />
           ))}
         </ul>
